6bb7726cdee00d5cf9fd5621218660829c63fbf0
joeygravlin <joeygravlin@gmail.com>
2015-11-03 17:18:19 -0500

    Formating changes:
    Convert tabs -> 4 spaces.
    Plus other misc formating changes.

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -2,55 +2,54 @@ package project3;
 
 import java.util.GregorianCalendar;
 
-public class CheckingAccount extends Account{
-	private static final long serialVersionUID = 1L;
-	private double monthlyFee;
+public class CheckingAccount extends Account {
+    private static final long serialVersionUID = 1L;
+    private double monthlyFee;
 
-	public CheckingAccount(){
-		
-	}
-	public CheckingAccount(int number, String owner, GregorianCalendar dateOpened, double balance) {
-		super(number, owner, dateOpened, balance);
-	}
+    public CheckingAccount() {
+    }
+    public CheckingAccount(int number, String owner, GregorianCalendar dateOpened, double balance) {
+        super(number, owner, dateOpened, balance);
+    }
 
-	public double getMonthlyFee() {
-		return monthlyFee;
-	}
+    public double getMonthlyFee() {
+        return monthlyFee;
+    }
 
-	public void setMonthlyFee(double monthlyFee) {
-		this.monthlyFee = monthlyFee;
-	}
+    public void setMonthlyFee(double monthlyFee) {
+        this.monthlyFee = monthlyFee;
+    }
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	
-	@Override
-	public String toString() {
-		return "CheckingAccount [monthlyFee=" + monthlyFee + "]";
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		long temp;
-		temp = Double.doubleToLongBits(monthlyFee);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CheckingAccount other = (CheckingAccount) obj;
-		if (Double.doubleToLongBits(monthlyFee) != Double.doubleToLongBits(other.monthlyFee))
-			return false;
-		return true;
-	}
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckingAccount [monthlyFee=" + monthlyFee + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(monthlyFee);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CheckingAccount other = (CheckingAccount) obj;
+        if (Double.doubleToLongBits(monthlyFee) != Double.doubleToLongBits(other.monthlyFee))
+            return false;
+        return true;
+    }
 }
--- accountOwnerComparison.java
+++ accountOwnerComparison.java
@@ -4,11 +4,11 @@ import java.util.Comparator;
 
 public class accountOwnerComparison implements Comparator<Account>{
 
-	@Override
-	public int compare(Account o1, Account o2) {
-		String name1 = o1.getOwner();
-		String name2 = o2.getOwner();
-		return name1.compareTo(name2);
-	}
+    @Override
+    public int compare(Account o1, Account o2) {
+        String name1 = o1.getOwner();
+        String name2 = o2.getOwner();
+        return name1.compareTo(name2);
+    }
 
 }
